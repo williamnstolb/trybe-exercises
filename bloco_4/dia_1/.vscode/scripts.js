e50c5efe0d5d855eec5454cdf36f6a497f1395df
Williamns <williamnstolb@gmail.com>
2021-06-23 18:20:04 -0300

    Questao 4 resolvida

--- bloco_4/dia_1/.vscode/scripts.js
+++ bloco_4/dia_1/.vscode/scripts.js
@@ -36,31 +36,81 @@ switch (operacao) {
 
 //Questão 2:
 
-if (a > b) {console.log("O maior valor é: ", a); situacao = a>b;}
-else if (a < b) {console.log("O maior valor é: ", b); situacao = a<b}
-else {console.log("Os dois valores são idênticos ou não são números, fica o mistério no ar!"); situacao = a==b}
+if (a > b) {
+  console.log("O maior valor é: ", a);
+  situacao = a > b;
+} else if (a < b) {
+  console.log("O maior valor é: ", b);
+  situacao = a < b
+} else {
+  console.log("Os dois valores são idênticos ou não são números, fica o mistério no ar!");
+  situacao = a == b
+}
 
 //Questão 3:
 
-switch (situacao){
-  case a>b:
-    if (a > c) {console.log("O maior valor é: ", a);}
-    else if (a < c) {console.log("O maior valor é: ", c);}
-    else {console.log("Os dois valores A e C são idênticos ou alguém não é um número, fica o mistério no ar!");}
+switch (situacao) {
+  case a > b:
+    if (a > c) {
+      console.log("O maior valor é: ", a);
+    } else if (a < c) {
+      console.log("O maior valor é: ", c);
+    } else {
+      console.log("Os dois valores A e C são idênticos ou alguém não é um número, fica o mistério no ar!");
+    }
     break;
 
-    case a<b:
-    if (b > c) {console.log("O maior valor é: ", b);}
-    else if (b < c) {console.log("O maior valor é: ", c);}
-    else {console.log("Os dois valores B e C são idênticos ou alguém não é um número, fica o mistério no ar!");}
+  case a < b:
+    if (b > c) {
+      console.log("O maior valor é: ", b);
+    } else if (b < c) {
+      console.log("O maior valor é: ", c);
+    } else {
+      console.log("Os dois valores B e C são idênticos ou alguém não é um número, fica o mistério no ar!");
+    }
     break;
 
-    case a==b:
-    if (a > c) {console.log("A e B são iguais e o maior valor é: ", a,);}
-    else if (a < c) {console.log("O maior valor é: ", c);}
-    else {console.log("Talvez todos os valores sejam iguais, ou talvez não. Jamais saberemos...");}
-      break;
-  
-    default:
-      console.log("Tem certeza que escolheu números dessa realidade para compará-los? Reveja seus conceitos!");
+  case a == b:
+    if (a > c) {
+      console.log("A e B são iguais e o maior valor é: ", a, );
+    } else if (a < c) {
+      console.log("O maior valor é: ", c);
+    } else {
+      console.log("Talvez todos os valores sejam iguais, ou talvez não. Jamais saberemos...");
+    }
+    break;
+
+  default:
+    console.log("Tem certeza que escolheu números dessa realidade para compará-los? Reveja seus conceitos!");
+}
+
+//Questão 4:
+
+let d = -5;
+let sinal;
+
+if (d > 0) {
+  sinal = "positive"
+} else if (d < 0) {
+  sinal = "negative"
+} else {
+  sinal = "zero"
+}
+
+switch (sinal) {
+  case "positive":
+    console.log("Este número é: ", sinal);
+    break;
+
+  case "negative":
+    console.log("Este número é: ", sinal);
+    break;
+
+  case "zero":
+    console.log("Este número é: ", sinal);
+    break;
+
+  default:
+    console.log("Talvez você tenha encontrado um novo padrão, fique feliz, mas verifique se não foi um erro!")
+
 }
