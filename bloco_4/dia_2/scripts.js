27e92af7378decf7b208fdf3d6031e9f28dc10ff
Williamns <williamnstolb@gmail.com>
2021-06-24 11:39:22 -0300

    2 resolvido

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -2,13 +2,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Questão 1:
 
-for (let index = 0; index < numbers.length; index += 1){
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
 
 //Questão 2:
 
-
+let somador = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  somador = somador + numbers[index];
+}
+console.log('A soma total é: ', somador);
 
 //Questão 3:
 
@@ -35,4 +39,3 @@ for (let index = 0; index < numbers.length; index += 1){
 
 
 //Questão 9:
-
