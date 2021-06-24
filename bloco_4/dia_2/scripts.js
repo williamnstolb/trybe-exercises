0cd47907aae4917db0d178a5e7cd9c670af1a5a7
Williamns <williamnstolb@gmail.com>
2021-06-24 12:07:25 -0300

    8 resolvido

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -59,6 +59,11 @@ console.log('O menor valor é: ', menorValor);
 
 //Questão 8:
 
-
+let lista = [];
+for (let index = 0; index < 25; index += 1) {
+  lista.push(index + 1);
+  console.log(lista[index]);
+}
 
 //Questão 9:
+
