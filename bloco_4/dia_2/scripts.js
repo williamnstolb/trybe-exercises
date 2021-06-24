6fdc0b8d16cc5b53fa4292761850bbb6115f8439
Williamns <williamnstolb@gmail.com>
2021-06-24 11:35:33 -0300

    1 resolvido

---
+++ bloco_4/dia_2/scripts.js
@@ -0,0 +1,38 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Questão 1:
+
+for (let index = 0; index < numbers.length; index += 1){
+  console.log(numbers[index]);
+}
+
+//Questão 2:
+
+
+
+//Questão 3:
+
+
+
+//Questão 4:
+
+
+
+//Questão 5:
+
+
+
+//Questão 6:
+
+
+
+//Questão 7:
+
+
+
+//Questão 8:
+
+
+
+//Questão 9:
+
