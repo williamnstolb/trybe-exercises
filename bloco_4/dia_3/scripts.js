61a5357f318b60a3ce455f1d7e582b55909aca36
Williamns <williamnstolb@gmail.com>
2021-06-25 18:12:59 -0300

    Resolvido 2

--- bloco_4/dia_3/scripts.js
+++ bloco_4/dia_3/scripts.js
@@ -1,6 +1,6 @@
 //Questão 1:
-
-let n = 2;
+console.log("Questão 1:")
+let n = 5;
 
 if (n > 0) {
   for (let i = 1; i <= n; i += 1) {
@@ -10,6 +10,19 @@ if (n > 0) {
     }
     console.log(listaVazia.join('  '));
   }
+} else {
+  console.log("Valor menor que 1.")
+}
+
+//Questão 2:
+console.log("Questão 2:")
+if (n > 0) {
+  let listaVazia = [];
+  for (let j = 1; j <= n; j += 1) {
+    listaVazia.push('*');
+    console.log(listaVazia.join('  '));
+  }
+
 } else {
   console.log("Valor menor que 1.")
 }
