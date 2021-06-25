676e556f52db7d353bdd90b498aef0317f10a5b1
Williamns <williamnstolb@gmail.com>
2021-06-25 20:04:27 -0300

    faltou a 4

--- bloco_4/dia_3/scripts.js
+++ bloco_4/dia_3/scripts.js
@@ -18,6 +18,7 @@ if (n > 0) {
 console.log("Questão 2:")
 if (n > 0) {
   let listaVazia = [];
+
   for (let j = 1; j <= n; j += 1) {
     listaVazia.push('*');
     console.log(listaVazia.join('  '));
@@ -32,6 +33,7 @@ console.log("Questão 3:")
 if (n > 0) {
   let listaVazia = [];
 
+  //Cria uma lista com espaços vazios.
   for (let j = 1; j <= n; j += 1) {
     listaVazia.push(' ');
   }
@@ -41,6 +43,28 @@ if (n > 0) {
     console.log(listaVazia.join('  '));
   }
 
+} else {
+  console.log("Valor menor que 1.")
+}
+
+//Questão 4:
+console.log("Questão 4:")
+
+if (n > 0) {
+  let listaVazia = [];
+
+  for (let j = 1; j <= n; j += 1) {
+
+    if (j % 2 != 0) {
+      listaVazia.push('*');
+      for (let i = 2; i >= 0; i -= 1) {
+        
+        listaVazia.unshift(' ');
+      }
+    }
+      console.log(listaVazia.join('  '));
+    
+  }
 } else {
   console.log("Valor menor que 1.")
 }
---
+++ bloco_4/dia_3/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (let i = 2; i >= 0; i -= 1) {
+
+      //   listaVazia.unshift(' ');
+      // }
