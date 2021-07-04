2afabeb564685bdf81edfa0f7a1cac9d5c776af1
Williamns <williamnstolb@gmail.com>
2021-07-04 19:32:05 -0300

    Tudo completo e funcionando 100%

--- fundamentos/bloco_5/dia_1/script.js
+++ fundamentos/bloco_5/dia_1/script.js
@@ -1,13 +1,13 @@
 function mudaTexto() {
   document.getElementsByTagName("p")[0].innerText = "1 ano após ter me formado na trybe e com toda a experiência acumulada com investimentos eu estou com um patrimônio bem satisfatório, melhor que as projeções que havia fetio. É uma maravilha trabalhar remotamente e meu inglês está melhorando.";
-};
-
-function mudaCorRetangulosExternos(cor) {
-  document.getElementsByClassName("main-content")[0].style.backgroundColor = "cor";
 }
 
-function mudaCorRetanguloInterno(cor) {
-  document.getElementsByClassName("center-content")[0].style.backgroundColor = "cor";
+function mudaCorRetangulosExternos() {
+  document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
+}
+
+function mudaCorRetanguloInterno() {
+  document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
 }
 
 function corrigeTitle() {
@@ -15,8 +15,14 @@ function corrigeTitle() {
 }
 
 function upperCase() {
-  for (let n = 0; n < document.getElementsByTagName("p"); n += 1) {
+  for (let n = 0; n < document.getElementsByTagName("p").length; n += 1) {
     document.getElementsByTagName("p")[n] = document.getElementsByTagName("p")[n].innerText.toUpperCase();
+  }
+}
+
+function exibeConteudo() {
+  for (let n = 0; n < document.getElementsByTagName("p").length; n += 1) {
+    console.log(document.getElementsByTagName("p")[n].innerText);
   }
 }
 
