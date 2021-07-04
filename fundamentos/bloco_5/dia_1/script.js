f664f84b6abf4e3efc30c75ffc1a22705f5bc79b
Williamns <williamnstolb@gmail.com>
2021-07-04 18:36:15 -0300

    item 1 e 2 feitos, mas nao funcionando 100%

---
+++ fundamentos/bloco_5/dia_1/script.js
@@ -0,0 +1,7 @@
+function mudaTexto(){
+  document.getElementsByTagName("p")[0].innerText = "1 ano após ter me formado na trybe e com toda a experiência acumulada com investimentos eu estou com um patrimônio bem satisfatório, melhor que as projeções que havia fetio. É uma maravilha trabalhar remotamente e meu inglês está melhorando.";
+};
+
+function mudaCorQuadradosExternos(cor){
+  document.getElementsByClassName("main-content")[0].style.backgroundColor = "cor";
+}
