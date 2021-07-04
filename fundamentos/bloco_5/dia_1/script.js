0976fed5ca70f94ddc23e46c2244b3431bd0818f
Williamns <williamnstolb@gmail.com>
2021-07-04 19:01:57 -0300

    item 5 feito, mas nao funcionando 100%

--- fundamentos/bloco_5/dia_1/script.js
+++ fundamentos/bloco_5/dia_1/script.js
@@ -1,15 +1,22 @@
-function mudaTexto(){
+function mudaTexto() {
   document.getElementsByTagName("p")[0].innerText = "1 ano após ter me formado na trybe e com toda a experiência acumulada com investimentos eu estou com um patrimônio bem satisfatório, melhor que as projeções que havia fetio. É uma maravilha trabalhar remotamente e meu inglês está melhorando.";
 };
 
-function mudaCorRetangulosExternos(cor){
+function mudaCorRetangulosExternos(cor) {
   document.getElementsByClassName("main-content")[0].style.backgroundColor = "cor";
 }
 
-function mudaCorRetanguloInterno(cor){
+function mudaCorRetanguloInterno(cor) {
   document.getElementsByClassName("center-content")[0].style.backgroundColor = "cor";
 }
 
-function corrigeTitle(){
+function corrigeTitle() {
   document.getElementsByClassName("title")[0].innerText = "Exercício 5.1 - JavaScript";
 }
+
+function upperCase() {
+  for (let n = 0; n < document.getElementsByTagName("p"); n += 1) {
+    document.getElementsByTagName("p")[n] = document.getElementsByTagName("p")[n].innerText.toUpperCase();
+  }
+}
+
