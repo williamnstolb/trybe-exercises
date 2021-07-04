caec0ec731c66ab24111ff6348747b829190af41
Williamns <williamnstolb@gmail.com>
2021-07-04 18:41:19 -0300

    item 3 feito, mas nao funcionando 100%

--- fundamentos/bloco_5/dia_1/script.js
+++ fundamentos/bloco_5/dia_1/script.js
@@ -2,6 +2,11 @@ function mudaTexto(){
   document.getElementsByTagName("p")[0].innerText = "1 ano após ter me formado na trybe e com toda a experiência acumulada com investimentos eu estou com um patrimônio bem satisfatório, melhor que as projeções que havia fetio. É uma maravilha trabalhar remotamente e meu inglês está melhorando.";
 };
 
-function mudaCorQuadradosExternos(cor){
+function mudaCorRetangulosExternos(cor){
   document.getElementsByClassName("main-content")[0].style.backgroundColor = "cor";
 }
+
+function mudaCorRetanguloInterno(cor){
+  document.getElementsByClassName("center-content")[0].style.backgroundColor = "cor";
+}
+
